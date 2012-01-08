8950e917f3536a76a0e1b1e1e7ec48209d40b191
Tane Piper <piper.tane@gmail.com>
2012-01-08 13:58:47 +0000

    Added support for settings items done and viewing list as active or done items

--- TodoManager.py
+++ TodoManager.py
@@ -4,35 +4,42 @@ CODEINTEL_HOME_DIR = os.path.expanduser(os.path.join('~', '.todomanager'))
 __file__ = os.path.normpath(os.path.abspath(__file__))
 __path__ = os.path.dirname(__file__)
 
+
+TASK_OPTIONS = [
+  ['', 'No priority'],
+  ['(A) ', 'Set a task to priority A'],
+  ['(B) ', 'Set a task to priority B']
+]
+
 def total_lines(fname):
   return sum(1 for line in open(fname)) + 1
 
 
 class TodoManagerAdd(sublime_plugin.WindowCommand):
 
+  ouput_string = ''
+
   def on_done(self, task):
+
+    self.ouput_string += task
+
     view = self.window.active_view()
     path = view.file_name().split(os.path.sep)
 
     todo_filename = 'todo-' + path[len(path) - 1] + '.txt'
     todo_path_file = open(todo_filename, 'a+')
 
-    todo_path_file.write("%s\n" %  task)
+    todo_path_file.write("%s\n" %  self.ouput_string)
     todo_path_file.close()
 
   def on_priority(self, priority):
-    priority_string = ''
-    if int(priority) == 1:
-      priority_string = '%s ' % ('(A)')
-    elif int(priority) == 2:
-      priority_string = '%s ' % ('(B)')
-    self.window.show_input_panel("Enter Task", '%s' % (priority_string), self.on_done, None, None)
+
+    self.ouput_string += TASK_OPTIONS[priority][0]
+
+    self.window.show_input_panel("Enter Task", '', self.on_done, None, None)
+
   def run(self):
-    self.window.show_quick_panel([
-      ['None', 'No priority'],
-      ['A', 'Set a task to priority A'],
-      ['B', 'Set a task to priority B']
-    ], self.on_priority)
+    self.window.show_quick_panel(TASK_OPTIONS, self.on_priority)
     #self.window.show_input_panel("Set Priority (Enter letter A/B/C/D", '', self.on_priority, None, None)
 
 
@@ -66,7 +73,17 @@ class TodoManagerList(sublime_plugin.WindowCommand):
       lines = open(todo_filename, 'r').readlines()
 
       self.window.show_input_panel("Edit Task", lines[option], self.on_edit, None, None)
+
     elif option == 1:
+      view = self.window.active_view()
+      path = view.file_name().split(os.path.sep)
+      todo_filename = 'todo-' + path[len(path) - 1] + '.txt'
+
+      lines = open(todo_filename, 'r').readlines()
+      lines[int(option)] = '* %s' % lines[int(option)]
+      open(todo_filename, 'w').writelines(lines)
+
+    elif option == 2:
       view = self.window.active_view()
       path = view.file_name().split(os.path.sep)
       todo_filename = 'todo-' + path[len(path) - 1] + '.txt'
@@ -77,19 +94,30 @@ class TodoManagerList(sublime_plugin.WindowCommand):
 
   def open_file(self, option):
     #pass
-    self.window.show_quick_panel(['Edit', 'Delete'], self.on_selection)
+    self.window.show_quick_panel(['Edit', 'Done', 'Delete'], self.on_selection)
 
   def on_list_cancel(self, option):
     pass
 
-  def run(self):
+  def run(self, show_done):
+    print show_done
     view = self.window.active_view()
     path = view.file_name().split(os.path.sep)
     todo_filename = 'todo-' + path[len(path) - 1] + '.txt'
 
     lines = open(todo_filename, 'r').readlines()
 
+    # Check for done items
+    active = []
+    for line in lines:
+      if show_done == 'true':
+        if line[:1] == '*':      
+          active.append(line)
+      else:
+        if line[:1] != '*':
+          active.append(line)
+
     if len(lines) > 0:
-      self.window.show_quick_panel(lines, self.open_file)
+      self.window.show_quick_panel(active, self.open_file)
     else:
       self.window.show_quick_panel(['No tasks for this file'], self.on_list_cancel)
