09f397795f3ea769ea513721019212a7b988b04b
Tane Piper <piper.tane@gmail.com>
2012-01-13 23:50:14 +0000

    Added settings and big refactor

--- TodoManager.py
+++ TodoManager.py
@@ -1,9 +1,17 @@
 import os, sublime, sublime_plugin, re
 
-TODOMANAGER_HOME_DIR = os.path.expanduser(os.path.join('~', '.todomanager'))
+DEFAULT_TODO_PATH = os.path.expanduser(os.path.join('~', '.todomanager'))
 
-if not os.path.exists(TODOMANAGER_HOME_DIR):
-  os.makedirs(TODOMANAGER_HOME_DIR)
+SHOW_STATE_ALL = 1
+SHOW_STATE_ACTIVE = 2
+SHOW_STATE_DONE = 3
+
+ACTION_DONE_STATE = 0
+ACTION_EDIT = 1
+ACTION_DELETE = 2
+
+if not os.path.exists(DEFAULT_TODO_PATH):
+  os.makedirs(DEFAULT_TODO_PATH)
 
 __file__ = os.path.normpath(os.path.abspath(__file__))
 __path__ = os.path.dirname(__file__)
@@ -17,263 +25,230 @@ TASK_OPTIONS = [
   ['(D) ', 'Set a todo to priority D']
 ]
 
-# Get the total number of lines in a file, add 1 because it's 0 index
-def total_lines(fname):
-  return sum(1 for line in open(fname)) + 1
 
-def get_file_name(command):
-  path = command.window.active_view().file_name().split(os.path.sep)
-  return '%s' % path[len(path) - 1]
+class TodoFile(object):
 
-# Get the full path of the todo text file
-def get_file_path(command):
-  path = command.window.active_view().file_name().split(os.path.sep)
-  return '%s\%s-%s.%s' % (TODOMANAGER_HOME_DIR, 'todo', path[len(path) - 1], 'txt')
+  parent_file_path = ''
 
-def create_header_line(line, line_index):
-  todo_header_string = 'Task %d' % line_index
+  parent_file_parts = None
 
-  match_level = re.match(r'\((\w)\)', line, re.I)
-  todo_header_string += ' %s' % match_level.group() if match_level else ''
+  parent_filename = ''
 
-  match_projects = re.compile('(\+\w+)')
-  for match in match_projects.finditer(line):
-    todo_header_string += ' %s' % match.group()
+  output_filepath = ''
 
-  match_contexts = re.compile('(\@\w+)')
-  for match in match_contexts.finditer(line):
-    todo_header_string += ' %s' % match.group()
+  lines = None
 
-  return todo_header_string
+  file_handler = None
 
+  total_todos = 0
 
+  active_todos = []
 
-class TodoManagerOpen(sublime_plugin.WindowCommand):
-  """
-  TodoManagerOpen opens the todo.txt file for the current open file
-  """
-  def run(self):
-    """
-    Open a new window with the todo file of the current open file
-    """
-    self.window.open_file(get_file_path(self))
+  done_todos = []
 
+  def __init__(self, command, settings):
+    self.parent_file_path = command.window.active_view().file_name()
+    self.parent_file_parts = self.parent_file_path.split(os.path.sep)
+    self.parent_filename = self.parent_file_parts[len(self.parent_file_parts) - 1]
 
-class TodoManagerAdd(sublime_plugin.WindowCommand):
-  """
-  TodoManagerAdd handles the flow of adding a new task to the file
-  """
+    self.output_filepath = '%s%s%s-%s.%s' % (DEFAULT_TODO_PATH, os.path.sep, 'todo', self.parent_filename, 'txt')
 
-  # The output string that will be saved to the text file
-  ouput_string = None
-
-  at_line = 0
-
-  def on_cancel(self):
-    pass
-
-  def on_done(self):
-    todo_path_file = open(get_file_path(self), 'a+')
-    todo_path_file.write("%s\n" %  self.ouput_string)
-    todo_path_file.close()
-
-    self.window.active_view().set_status('todomanager', 'Todo saved: ' + self.ouput_string)
-
-  def on_contexts(self, contexts):
-    if contexts != '':
-      contexts_list = contexts.split(' ')
-      for context in contexts_list:
-        self.ouput_string += ' @%s' % context
-
-    self.on_done()
-
-  def on_projects(self, projects):
-    if projects != '':
-      projects_list = projects.split(' ')
-      for project in projects_list:
-        self.ouput_string += ' +%s' % project
-
-    self.window.show_input_panel("Enter Contexts", '', self.on_contexts, None, self.on_cancel)
-
-
-  def on_line_number(self, line_number):
-    if line_number != '':
-      self.ouput_string += ' ~%s' % line_number
-
-    self.window.show_input_panel("Enter Projects", '', self.on_projects, None, self.on_cancel)
-
-  def on_task(self, task):
-    if task != '':
-      self.ouput_string += task
-      self.window.show_input_panel("Enter Line Number", str(self.at_line) if self.at_line > 0 else '', self.on_line_number, None, self.on_cancel)
-    else:
-      pass
-
-  def on_priority(self, priority):
-    self.ouput_string += TASK_OPTIONS[priority][0]
-    self.window.show_input_panel("Enter Todo", '', self.on_task, None, self.on_cancel)
-
-  # Get the user to set the priority first
-  def run(self, at_line):
-    self.at_line = 0
-    line, column = self.window.active_view().rowcol(self.window.active_view().sel()[0].begin())
-    if at_line is True:
-      self.at_line = line
-    self.ouput_string = ''
-    self.window.show_quick_panel(TASK_OPTIONS, self.on_priority)
-
-
-class TodoManagerEdit(sublime_plugin.WindowCommand):
-
-  current_task_position = None
-
-  def on_cancel(self, task):
-    pass
-
-  def on_task(self, task):
-    lines = open(get_file_path(self), 'r').readlines()
-    lines[self.current_task_position] = '%s' % task
-    open(get_file_path(self), 'w').writelines(lines)
-
-  def on_task_selection(self, task):
-    self.current_task_position = task
-    lines = open(get_file_path(self), 'r').readlines()
-    self.window.show_input_panel("Edit Todo", lines[int(task)], self.on_task, None, self.on_cancel)
-
-  def run(self):
-    self.current_task_position = None
-    lines = None
+    print self.output_filepath
     try:
-      lines = open(get_file_path(self), 'r').readlines()
+      if not os.path.exists(DEFAULT_TODO_PATH):
+        os.makedirs(DEFAULT_TODO_PATH)
+      self.open_file('r')
+      self.lines = self.file_handler.readlines()
+      self.close_file()
+
+      self.process_lines()
+
     except IOError:
-      self.window.show_quick_panel(['No todos for this file'], self.on_cancel)
+      return false
+    #path_parts = command.window.active_view().file_name().split(os.path.sep)
+    #self.filename = '%s' % path_parts[len(path_parts) - 1]
 
-    if lines:
-      self.window.show_quick_panel(lines, self.on_task_selection)
+  def process_lines(self):
+    self.total_todos = sum(1 for line in self.lines)
+
+    for line in self.lines:
+      if line[:1] == '*':
+        self.done_todos.append(line)
+      else:
+        self.active_todos.append(line)
+
+  def open_file(self, mode):
+    self.file_handler = open(self.output_filepath, mode)
+
+  def close_file(self):
+    self.file_handler.close()
+
+  def write(self):
+    self.open_file('w')
+    self.file_handler.writelines(self.lines)
+    self.close_file()
+
+  # def write_file(self):
+  #   self.open_file('w')
+  #   line_position = self.line_mappings[self.task_position][1]
+  #   line = self.lines[line_position]
+
+  def create_header_line(self, line, line_index):
+    todo_header_string = 'Task %d' % line_index
+
+    match_level = re.match(r'\((\w)\)', line, re.I)
+    todo_header_string += ' %s' % match_level.group() if match_level else ''
+
+    match_projects = re.compile('(\+\w+)')
+    for match in match_projects.finditer(line):
+      todo_header_string += ' %s' % match.group()
+
+    match_contexts = re.compile('(\@\w+)')
+    for match in match_contexts.finditer(line):
+      todo_header_string += ' %s' % match.group()
+
+    return todo_header_string
+
+  def generate_list(self, show_state):
+    self.current_line_index = 0
+
+    self.current_display_mapping = []
+    self.current_display_items = []
+
+    for line in self.lines:
+      if show_state == SHOW_STATE_DONE and line[:1] == '*':
+        self.current_display_mapping.append(self.current_line_index)
+        self.current_display_items.append([self.create_header_line(line, self.current_line_index), line])
+      elif show_state == SHOW_STATE_ACTIVE and line[:1] != '*':
+        self.current_display_mapping.append(self.current_line_index)
+        self.current_display_items.append([self.create_header_line(line, self.current_line_index), line])
+      elif show_state == SHOW_STATE_ALL:
+        self.current_display_mapping.append(self.current_line_index)
+        self.current_display_items.append([self.create_header_line(line, self.current_line_index), line])
+      self.current_line_index = self.current_line_index + 1
+
+    if len(self.current_display_items) > 0:
+      return self.current_display_items
     else:
-      pass
+      return ['No todos for this file', 'Select the Todo: Add option to begin adding']
 
+  def mark_task(self, task_number):
 
-class TodoManagerDelete(sublime_plugin.WindowCommand):
-
-  def on_cancel(self, task):
-    pass
-
-  def on_task_selection(self, task):
-    lines = open(get_file_path(self), 'r').readlines()
-    del lines[int(task)]
-    open(get_file_path(self), 'w').writelines(lines)
-
-  def run(self):
-    lines = None
-    try:
-      lines = open(get_file_path(self), 'r').readlines()
-    except IOError:
-      self.window.show_quick_panel(['No todos for this file'], self.on_cancel)
-
-    if lines:
-      self.window.show_quick_panel(lines, self.on_task_selection)
+    line_number = self.current_display_mapping[task_number]
+    line = self.lines[line_number]
+    if line[:1] != '*':
+      self.lines[line_number] = '* %s' % line
+    elif line[:1] == '*':
+      self.lines[line_number] = '%s' % line.replace('* ', '')
     else:
-      pass
+      self.lines[line_number] = '%s' % line
+    self.write()
+
+  def get_line(self, task_number):
+    line_number = self.current_display_mapping[task_number]
+    line = self.lines[line_number]
+    return line
+
+  def edit_task(self, task_number, new_line):
+    line_number = self.current_display_mapping[task_number]
+    self.lines[line_number] = '%s' % new_line
+    self.write()
+
+  def delete_task(self, task_number):
+    line_number = self.current_display_mapping[task_number]
+    del self.lines[line_number]
+    self.write()
+
 
 
 class TodoManagerList(sublime_plugin.WindowCommand):
   """
-  The class that deals will all functions when a user wants to list todo items.
-  As well as displaying todos, this is also a jumping point off to managing
-  todos, such as marking them done, editing or deleting them
+  WindowCommand for the following features:
+    * Todo: All
+    * Todo: Active
+    * Todo: Done
+  These three options generate the list of items that are displayed.  Contexts
+  and projects are extracted from them to aid search, and clicking on a list item
+  gives you a futher three options
+    * Mark (Un)Done - Toggle the done/undone state of the task
+    * Edit - Edit the task in raw mode
+    * Delete - Delete the task
   """
 
-  # The lines that have been read in from the todo file
-  lines = None
+  # The show state of the view is one of SHOW_STATE_ALL, SHOW_STATE_ACTIVE and
+  # SHOW_STATE_DONE
+  show_state = 0
 
-  # List to contain the mapping of lines to quick panel entries
+  # The todo file object that is generated
+  todo_file = None
+
+  # Line mappings for display to real lines
   line_mappings = []
 
-  # The currently selected task position in the quick panel
+  # The current selected task position for option functions
   task_position = None
 
-  def on_cancel(self, task):
+  def on_edit_task(self, new_task_line):
     """
-    Function to be called when a request is cancelled
+    When the user has edited the task this method is called to edit
+    the line and save the file
+    """
+    self.todo_file.edit_task(self.task_position, new_task_line)
+
+  def on_cancel(self):
+    """
+    Generic cancel handler
     """
     pass
 
-  def on_task(self, task):
+  def on_task_action(self, option):
     """
-
+    Calls the appropriate functionality to continue based on action selection. Action is
+    one of ACTION_DONE_STATE, ACTION_EDIT or ACTION_DELETE
     """
-    line_position = self.line_mappings[self.task_position][1]
-    self.lines[line_position] = '%s' % task
-    open(get_file_path(self), 'w').writelines(self.lines)
+    if option == -1:
+      pass
+    elif option == ACTION_DONE_STATE:
+      self.todo_file.mark_task(self.task_position)
+    elif option == ACTION_EDIT:
+      self.window.show_input_panel("Edit Todo",  self.todo_file.get_line(self.task_position), self.on_edit_task, None, self.on_cancel)
+    elif option == ACTION_DELETE:
+      self.todo_file.delete_task(self.task_position)
+     
 
-  def on_action(self, action):
-    if action == 0:
-      line_position = self.line_mappings[self.task_position][1]
-      line = self.lines[line_position]
-      if line[:1] == '*':
-        self.lines[line_position] = '%s' % self.lines[line_position].replace('* ', '')
-      else:
-        self.lines[line_position] = '* %s' % self.lines[line_position]
 
-      open(get_file_path(self), 'w').writelines(self.lines)
-    if action == 1:
-      line_position = self.line_mappings[self.task_position][1]
-      line = self.lines[line_position]
-      self.window.show_input_panel("Edit Todo", line, self.on_task, None, self.on_cancel)
-    if action == 2:
-      line_position = self.line_mappings[self.task_position][1]
-      del self.lines[int(line_position)]
-      open(get_file_path(self), 'w').writelines(self.lines)
-
-  def on_task_selection(self, task_position):
-    self.task_position = task_position
-    self.window.show_quick_panel(['Mark (Un)Done', 'Edit', 'Delete'], self.on_action)
-
-  def run(self, show_done):
+  def on_task_selection(self, option):
     """
-    Function called when a command is passed, takes a parameter of show_done
-    which decides if the user wants to display done or active items
+    When a task from the presented list has been selected, the action selection menu is shown
     """
+    if option == -1:
+      pass
+    self.task_position = option
+    self.window.show_quick_panel(['Mark (Un)Done', 'Edit', 'Delete'], self.on_task_action)
 
-    # Reset the lines to none
-    self.lines = None
+  def run(self, show_state):
+    """
+    Resets all the values of the plugin for this run, loads settings
+    and the todo file (or creates it if it doesn't exist)
+    Then presents a quick panel with the generated list.
 
-    # Reset the line mappings to a empty list
+    Takes argument for show state which determines if SHOW_STATE_ALL, SHOW_STATE_ACTIVE
+    or SHOW_STATE_DONE
+    """
+    # Reset values
+    self.show_state = show_state
+
     self.line_mappings = []
 
-    # Open a file or display a message about no file
-    try:
-      self.lines = open(get_file_path(self), 'r').readlines()
-    except IOError:
-      sublime.error_message('There is no todo file for %s' % get_file_name(self))
-      return
+    self.task_position = None
 
-    # Keep a list of active lines for display
-    active_lines = []
-    # Check for done items
-    panel_index = 0
-    # Keep a note of the line index
-    line_index = 0
-    text = ''
-    # Loop over the line
-    for line in self.lines:
-      todo_header_string = ''
-      if show_done == True:
-        text = 'done'
-        if line[:1] == '*':
-          self.line_mappings.append([panel_index, line_index])
-          active_lines.append([create_header_line(line, line_index), line])
-          panel_index = panel_index + 1
-      else:
-        text = 'active'
-        if line[:1] != '*':
-          self.line_mappings.append([panel_index, line_index])
-          active_lines.append([create_header_line(line, line_index), line])
-          panel_index = panel_index + 1
-      line_index = line_index + 1
+    settings = sublime.load_settings('TodoManager.sublime-settings')
 
-    if len(active_lines) > 0:
-      self.window.show_quick_panel(active_lines, self.on_task_selection)
+    todo = TodoFile(self, settings)
+    if todo:
+      self.todo_file = todo
     else:
-      sublime.error_message('There are no %s todos for %s' % ( text, get_file_name(self) ) )
+      sublime.error_message('Error opening file for %s' % self.window.active_view().file_name())
+      pass
+    self.window.active_view().set_status('todomanager', 'Total active todos: %d Total done todos: %s Total todos: %d' % ( len(self.todo_file.active_todos), len(self.todo_file.done_todos), self.todo_file.total_todos ))
+    self.window.show_quick_panel(self.todo_file.generate_list(self.show_state), self.on_task_selection)
